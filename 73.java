cdfc2ab5420fbbaef569ba324b1e5acce16d2ba2
Viswa Sunny <viswasunny007@gmail.com>
2018-08-21 11:34:48 +0530

    Create 73.java

---
+++ 73.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int l=s.nextInt();
+		int r=s.nextInt();
+		if(n>l && n<r)
+		System.out.println("yes");
+		else
+		System.out.println("no");
+	}
+}
