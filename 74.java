070bc91a9d0d6c293c82b0c1ac6323d4d6c32814
Viswa Sunny <viswasunny007@gmail.com>
2018-08-21 11:42:39 +0530

    Create 74.java

---
+++ 74.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Guvi
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		float n=s.nextFloat();
+		System.out.println((int)Math.ceil(n));
+	}
+}
