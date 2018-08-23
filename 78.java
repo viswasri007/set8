10884b8464d32bca26dbbad442a1c1b9074b8aa0
Viswa Sunny <viswasunny007@gmail.com>
2018-08-23 09:28:47 +0530

    Create 78.java

---
+++ 78.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Guvi
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		if(n%13==0)
+		{
+			System.out.println("yes");
+		}
+		else
+		{
+			System.out.println("no");
+		}
+	}
+}
